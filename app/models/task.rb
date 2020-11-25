1cfad0bd9866b956a8f58d433a5e26040a3fd295
Mahir Mersimoski <m.mersimoski1999@gmail.com>
2020-11-25 23:34:19 +0100

    initial commit

---
+++ app/models/task.rb
@@ -0,0 +1,38 @@
+class Task < ApplicationRecord
+  belongs_to :project
+
+  validates :status, inclusion: {in: ['not-started', "in-progress", "completed"]}
+
+  STATUS_OPTIONS = [['Not started', 'not-started'],
+  ['In progress', 'in-progress'],
+  ['Completed', 'completed']]
+
+
+ def badge_color
+ 	case status
+ 	when 'not-started'
+ 		'secondary'
+ 	when 'in-progress'
+ 		'info'
+ 	when 'completed'
+ 		'success'
+	end
+end
+
+def completed?
+	status == 'completed'
+	
+end
+
+def in_progress?
+	status == 'in-progress'
+	
+end
+
+def not_started?
+	status == 'not-started'
+	
+end
+
+
+end
